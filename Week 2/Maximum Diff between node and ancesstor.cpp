933b53566883266afb7a9c521cc5dc25a5cf7014
Madhav Sharma <madhav9620@gmail.com>
2020-11-09 18:57:26 +0000

    TREE

---
+++ Week 2/Maximum Diff between node and ancesstor.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solve(TreeNode* root,int max_till_now,int min_till_now,int &res){
+        if(root==NULL){
+            return;
+        }
+        res=max(abs(max_till_now-root->val),res);
+        res=max(abs(min_till_now-root->val),res);
+        
+        max_till_now=max(root->val,max_till_now);
+        min_till_now=min(root->val,min_till_now);
+        
+        solve(root->left,max_till_now,min_till_now,res);
+        solve(root->right,max_till_now,min_till_now,res);
+    }
+    int maxAncestorDiff(TreeNode* root) {
+        if(NULL == root || (NULL ==root->left && NULL == root->right))
+            return 0;
+        int max_till_now=root->val;
+        int min_till_now=root->val;
+        int res=0;
+        
+        solve(root->left,max_till_now,min_till_now,res);
+        solve(root->right,max_till_now,min_till_now,res);
+        return res;
+    }
+};
